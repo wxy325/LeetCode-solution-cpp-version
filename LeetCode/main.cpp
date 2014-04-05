a58a0cb8c99b3dc219fde4a00e9d2d29f4eb89a7
wxy325 <wxy325@qq.com>
2014-04-05 22:46:30 +0800

    Initial Commit

---
+++ LeetCode/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LeetCode
+//
+//  Created by wxy325 on 4/5/14.
+//  Copyright (c) 2014 wxy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
